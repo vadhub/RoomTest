f1014fc6286de2d00990007ed7947a9011b5b295
vadhub <gabderahmanov99@gmail.com>
2022-03-12 13:51:09 +0300

    add foreingkey in table user

--- app/src/main/java/com/vad/roomtest/room/dao/User.kt
+++ app/src/main/java/com/vad/roomtest/room/dao/User.kt
@@ -2,10 +2,22 @@ package com.vad.roomtest.room.dao
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.vad.roomtest.room.dao.User.Companion.TABLE_NAME
 
-@Entity(tableName = TABLE_NAME)
+@Entity(
+    tableName = TABLE_NAME,
+    foreignKeys = [
+        ForeignKey(
+            entity = Work::class,
+            parentColumns = ["work_id"],
+            childColumns = ["id_work"],
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+    )
 data class User(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "user_id")
@@ -14,8 +26,8 @@ data class User(
     @ColumnInfo(name = "name")
     val name: String,
 
-    @ColumnInfo(name = "work")
-    val Works: Work
+    @ColumnInfo(name = "id_work")
+    val idWork: Int
 ) {
     companion object {
         const val TABLE_NAME = "user_table"
