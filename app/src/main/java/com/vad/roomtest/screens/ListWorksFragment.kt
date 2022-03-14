2f188d154495e7ea574091bad158911b0799f1cf
vadhub <gabderahmanov99@gmail.com>
2022-03-14 21:59:46 +0300

    initialize list of works

--- app/src/main/java/com/vad/roomtest/screens/ListWorksFragment.kt
+++ app/src/main/java/com/vad/roomtest/screens/ListWorksFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vad.roomtest.R
+import com.vad.roomtest.room.dao.Work
 
 class ListWorksFragment : Fragment() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(requireActivity()).get(WorksViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_list_works, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_list_works, container, false)
+
+        val recycler = v.findViewById<RecyclerView>(R.id.listWorksRecycler)
+        val adapterListWorks = AdapterListWorks()
+
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapterListWorks
+
+        viewModel.getWorks.observe(viewLifecycleOwner) {
+            adapterListWorks.setWorks(it)
+        }
+
+        return v
     }
 }
