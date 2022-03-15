8993cae3fac96d0032d316c80a4a5ad7d8462cb1
vadhub <gabderahmanov99@gmail.com>
2022-03-15 20:08:54 +0300

    initialize database

--- app/src/main/java/com/vad/roomtest/screens/WorksViewModel.kt
+++ app/src/main/java/com/vad/roomtest/screens/WorksViewModel.kt
@@ -1,16 +1,24 @@
 package com.vad.roomtest.screens
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.vad.roomtest.App
+import com.vad.roomtest.room.dao.DaoWorks
 import com.vad.roomtest.room.dao.Work
+import com.vad.roomtest.room.database.AppDatabase
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class WorksViewModel : ViewModel() {
-    private val daoWork = App.instance.daoWorks
-    val getWorks: LiveData<List<Work>> = daoWork.getAllWorks()
+class WorksViewModel(application: Application) : ViewModel() {
+
+    private val daoWork: DaoWorks
+    val getWorks: LiveData<List<Work>>
+
+    init {
+        daoWork = AppDatabase.getDatabase(application).daoWorks()
+        getWorks = daoWork.getAllWorks()
+    }
 
     fun addWork(work: Work) = runBlocking {
         coroutineScope {
