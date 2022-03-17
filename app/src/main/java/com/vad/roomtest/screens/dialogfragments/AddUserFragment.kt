27e6f77ca92cce56462a79db1cfe1995849f13c7
vadhub <gabderahmanov99@gmail.com>
2022-03-17 11:45:23 +0300

    create dialog fragment add user

---
+++ app/src/main/java/com/vad/roomtest/screens/dialogfragments/AddUserFragment.kt
@@ -0,0 +1,19 @@
+package com.vad.roomtest.screens.dialogfragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.vad.roomtest.R
+
+class AddUserFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_add_user, container, false)
+        return v
+    }
+}
