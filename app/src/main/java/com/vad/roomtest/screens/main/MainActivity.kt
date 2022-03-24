55fe98723c457682035b183e48e4449da557d0a2
vadhub <gabderahmanov99@gmail.com>
2022-03-24 13:32:30 +0300

    add selected item listener

--- app/src/main/java/com/vad/roomtest/screens/main/MainActivity.kt
+++ app/src/main/java/com/vad/roomtest/screens/main/MainActivity.kt
@@ -2,16 +2,31 @@ package com.vad.roomtest.screens.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import com.vad.roomtest.R
+import com.vad.roomtest.screens.listusersfragment.ListUsersFragment
 import com.vad.roomtest.screens.listworksfragment.ListWorksFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var currentFragment: Fragment
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ListWorksFragment()).commit()
+        bottomNavView = findViewById(R.id.menu_nav)
+
+        bottomNavView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navUsers -> currentFragment = ListUsersFragment()
+                R.id.navWorks -> currentFragment = ListWorksFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, currentFragment).commit()
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
