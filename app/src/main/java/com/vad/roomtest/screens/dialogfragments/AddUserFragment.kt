f5162092b30317d7f623a00981dcb9fb29fd43de
vadhub <gabderahmanov99@gmail.com>
2022-03-25 21:08:59 +0300

    available add user

--- app/src/main/java/com/vad/roomtest/screens/dialogfragments/AddUserFragment.kt
+++ app/src/main/java/com/vad/roomtest/screens/dialogfragments/AddUserFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import com.vad.roomtest.R
+import com.vad.roomtest.room.dao.User
 import com.vad.roomtest.screens.listusersfragment.UsersViewModel
 import com.vad.roomtest.screens.listworksfragment.WorksViewModel
 
@@ -21,33 +19,35 @@ class AddUserFragment(private val viewModel: UsersViewModel, private val viewMod
         savedInstanceState: Bundle?
     ): View? {
         val v = inflater.inflate(R.layout.fragment_add_user, container, false)
-
+        var idWork = 0
         val spinner = v.findViewById<Spinner>(R.id.workListSpinner)
-        var works = ArrayList<String>()
+        val nameText = v.findViewById<EditText>(R.id.nameUser)
 
         viewModelWorks.getWorks.observe(viewLifecycleOwner) {
-            works = it.map { it.nameWork } as ArrayList<String>
-        }
+            val works = it.map { it.nameWork } as ArrayList<String>
 
-        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, works.toArray())
-        spinner.adapter = arrayAdapter
-        spinner.setSelection(1)
+            val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, works.toArray())
+            spinner.adapter = arrayAdapter
+            spinner.setSelection(1)
 
-        println(works.toString())
-        spinner.onItemSelectedListener = object: OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                println(p2)
-                println(works.get(p2))
+            println(works.toString())
+            spinner.onItemSelectedListener = object: OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    idWork = p2
+                    println(p2)
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
+            println(idWork)
 
+            v.findViewById<Button>(R.id.addUserAndWorkBtn).setOnClickListener {
+                dialog?.dismiss()
+                viewModel.addUser(User(0, nameText.text.toString(), idWork))
             }
 
-        }
-
-        v.findViewById<Button>(R.id.addUserAndWorkBtn).setOnClickListener {
-            dialog?.dismiss()
         }
 
         return v
